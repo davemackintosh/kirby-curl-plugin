409e4ed71d7bed0a381f3024f91582f2e35e3fe5
davemackintosh <dave.mackintosh@dotankstudios.com>
2013-02-08 14:31:45 +0000

    doc blocks 'n stuff!

--- curl.php
+++ curl.php
@@ -20,28 +20,47 @@ class kurl {
         return $inst;
     }
     
+    /**
+     * Sets the url for the request
+     */
     public function url ($url) {
     	curl_setopt($this->connection, CURLOPT_URL, $url);
     	return $this;
     }
         
+    /**
+	 * Whether or not to verify the host and peer
+	 *
+	 * @param $bool = boolean | 1 | 0
+	 */
     public function verify ($bool = false) {
     	curl_setopt($this->connection, CURLOPT_SSL_VERIFYHOST, $bool);
     	curl_setopt($this->connection, CURLOPT_SSL_VERIFYPEER, $bool);
     	return $this;
     }
 
+    /**
+     * Post an array of data to the url
+     */
 	public function post (array $data) {
 		curl_setopt($this->connection, CURLOPT_POST, count($data));
 		curl_setopt($this->connection, CURLOPT_POSTFIELDS, http_build_query($data));
 		return $this;
 	}
 
+	/**
+	 * Set the flag to return the data
+	 */
 	public function returnData () {
 		curl_setopt($this->connection, CURLOPT_RETURNTRANSFER, true);
 		return $this;
 	}
 
+	/**
+	 * Execute and fire the callback function if supplied
+	 *
+	 * @return the results of the curl request
+	 */
 	public function execute ($callback = null) {
 		$out = curl_exec($this->connection);
 		$err = curl_error($this->connection);
